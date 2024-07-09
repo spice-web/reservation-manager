e651ac03e67ce0e0a72dccb41b665e424ad1b007
miton310 <blue.blue210@gmail.com>
2024-07-09 11:34:29 +0900

    add tailwindcss

--- src/marketing_support/inventory_graph.php
+++ src/marketing_support/inventory_graph.php
@@ -18,6 +18,13 @@
       <div class="l-container__inner">
         <!-- 表示期間選択 -->
         <div class="l-flex l-flex--sb l-flex--item-end l-grid--gap2 u-mb2">
+          <!-- 月別 週別 時間別 任意の期間 button-->
+          <div class="l-grid--col4-auto l-grid--gap05 u-mb1">
+            <button class="c-button__apply u-w90">月別</button>
+            <button class="c-button__apply u-w90">週別</button>
+            <button class="c-button__apply u-w-auto">時間別</button>
+            <button class="c-button__apply u-w-auto">任意の期間</button>
+          </div>
           <div>
             <div>
               <label for="start">表示期間</label>
@@ -34,33 +41,19 @@
             </div>
           </div>
 
-          <!-- 月別 週別 時間別 任意の期間 button-->
-          <div class="l-grid--col4-auto l-grid--gap05 u-mb1">
-            <button class="c-button__apply u-w90">月別</button>
-            <button class="c-button__apply u-w90">週別</button>
-            <button class="c-button__apply u-w-auto">時間別</button>
-            <button class="c-button__apply u-w-auto">任意の期間</button>
-          </div>
+
         </div>
 
         <!-- グラフ -->
-        <div class="c-graph__wrap">
-          <div class="c-button__next"></div>
+        <div class="relative c-graph__wrap">
+          <div class="c-button__prev absolute-VerticalCenter"></div>
+          <div class="c-button__next absolute-VerticalCenter--right"></div>
         </div>
         <!-- グラフ -->
 
       </div>
     </main><!-- /.l-container__main -->
   </div>
-
-  <!-- webSplides Required -->
-  <script src="/js/webslides.min.js"></script>
-  <script>
-    window.ws = new WebSlides();
-  </script>
-
-  <!-- OPTIONAL - svg-icons.js (fontastic.me - Font Awesome as svg icons) -->
-  <script defer src="/js/svg-icons.js"></script>
 </body>
 
 </html>
--- src/marketing_support/reservation_performance_graph.php
+++ src/marketing_support/reservation_performance_graph.php
@@ -18,6 +18,13 @@
       <div class="l-container__inner">
         <!-- 表示期間選択 -->
         <div class="l-flex l-flex--sb l-flex--item-end l-grid--gap2 u-mb2">
+          <!-- 月別 週別 時間別 任意の期間 button-->
+          <div class="l-grid--col4-auto l-grid--gap05 u-mb1">
+            <button class="c-button__submit u-w90">月別</button>
+            <button class="c-button__submit u-w90">週別</button>
+            <button class="c-button__submit u-w-auto">時間別</button>
+            <button class="c-button__submit u-w-auto">任意の期間</button>
+          </div>
           <div>
             <div>
               <label for="start">表示期間</label>
@@ -33,68 +40,19 @@
               <input type="date" name="compare" id="compare_end" class="u-w210">
             </div>
           </div>
-
-          <!-- 月別 週別 時間別 任意の期間 button-->
-          <div class="l-grid--col4-auto l-grid--gap05 u-mb1">
-            <button class="c-button__submit u-w90">月別</button>
-            <button class="c-button__submit u-w90">週別</button>
-            <button class="c-button__submit u-w-auto">時間別</button>
-            <button class="c-button__submit u-w-auto">任意の期間</button>
-          </div>
         </div>
 
         <!-- グラフ -->
-        <!-- PyScriptの読み込み  -->
-        <link rel="stylesheet" href="https://pyscript.net/releases/2023.11.2/core.css" />
-        <script type="module" src="https://pyscript.net/releases/2023.11.2/core.js"></script>
-        <py-config>packages = ["matplotlib"]</py-config>
-        <script type="py">
-          from pyscript import display
-          import matplotlib.pyplot as plt
-          import numpy as np
-
-          x = np.linspace(0,10,100)
-          y = np.sin(x)
-          fig = plt.figure()
-          plt.plot(x,y,c="r",label=r"sine")
-          plt.grid()
-          plt.legend()
-
-          display(plt, target="mlp")
-        </script>
-        <div class="c-graph__wrap">
-          <!-- <article id="webslides"></article>の中にグラフ挿入 -->
-          <article id="webslides">
-            <!-- Slide 1 -->
-            <section>
-              <!-- それぞれを<section>で囲む -->
-            </section>
-            <!-- Slide 2 -->
-            <section class="bg-primary">
-              <div class="wrap">
-                <h2>.wrap = container (width: 90%)</h2>
-
-                <!-- グラフの描画先を指定 --- (*2) -->
-                <div id="mlp" align="center"></div>
-
-              </div>
-            </section>
-          </article>
-
-
+        <div class="relative c-graph__wrap">
+          <div class="c-button__prev absolute-VerticalCenter"></div>
+          <div class="c-button__next absolute-VerticalCenter--right"></div>
         </div>
+        <!-- グラフ -->
+
       </div>
     </main><!-- /.l-container__main -->
   </div>
 
-  <!-- webSplides Required -->
-  <script src="/js/webslides.min.js"></script>
-  <script>
-    window.ws = new WebSlides();
-  </script>
-
-  <!-- OPTIONAL - svg-icons.js (fontastic.me - Font Awesome as svg icons) -->
-  <script defer src="/js/svg-icons.js"></script>
 </body>
 
 </html>
