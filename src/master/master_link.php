4b0b279363efb3e301b4b8da152bbf9ac0f9520f
miton310 <blue.blue210@gmail.com>
2024-11-15 12:57:00 +0900

    make master/master_link.php

---
+++ src/master/master_link.php
@@ -0,0 +1,40 @@
+<!-- マスタ登録 -->
+<!-- register.php -->
+<!-- G-0 -->
+<!DOCTYPE html>
+<html lang="ja">
+<?php include_once '../include/head.php'; ?>
+
+<body>
+  <div class="l-wrap">
+    <?php include_once '../include/header.php'; ?>
+
+    <?php include_once '../include/nav.php'; ?>
+
+    <main class="l-wrap__main">
+      <!-- パンくず -->
+      <ul class="l-wrap__breadcrumb l-breadcrumb">
+        <li class="l-breadcrumb__list">マスタ設定一覧</li>
+      </ul>
+
+      <div class="l-container__inner">
+        <div class="l-grid--col5 l-grid--rgap2">
+          <a href="" class="c-button__submit c-button--yellow c-link-no-border">航空会社マスタ</a>
+          <a href="" class="c-button__submit c-button--yellow c-link-no-border">航空会社マスタ</a>
+          <a href="" class="c-button__submit c-button--yellow c-link-no-border">航空会社マスタ</a>
+          <a href="" class="c-button__submit c-button--yellow c-link-no-border">航空会社マスタ</a>
+          <a href="" class="c-button__submit c-button--yellow c-link-no-border">航空会社マスタ</a>
+          <a href="" class="c-button__submit c-button--yellow c-link-no-border">航空会社マスタ</a>
+          <a href="" class="c-button__submit c-button--yellow c-link-no-border">航空会社マスタ</a>
+          <a href="" class="c-button__submit c-button--yellow c-link-no-border">航空会社マスタ</a>
+          <a href="" class="c-button__submit c-button--yellow c-link-no-border">航空会社マスタ</a>
+          <a href="" class="c-button__submit c-button--yellow c-link-no-border">航空会社マスタ</a>
+          <a href="" class="c-button__submit c-button--yellow c-link-no-border">航空会社マスタ</a>
+        </div>
+      </div><!-- ./l-container__inner -->
+    </main>
+
+  </div>
+</body>
+
+</html>
